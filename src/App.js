082c4cdc3f9e22e2707c83a389876464e41fd6c2
Alex <velengura.alex@gmail.com>
2023-02-17 11:34:02 +0200

    synced progress

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
-
+import CostItem from "./components/CostItem";
+const costs = [
+  {
+    date: new Date(2021, 2, 12),
+    description: "Fridge",
+    amount: 999.99
+  },
+  {
+    date: new Date(2021, 3, 1),
+    description: "Laptop",
+    amount: 1200
+  },
+  {
+    date: new Date(2021, 11, 11),
+    description: "Jeans",
+    amount: 49.99
+  }
+];
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+     <h1>Hello world</h1>
+     <CostItem date={costs[0].date} description={costs[0].description} amount={costs[0].amount} />
+     <CostItem date={costs[1].date} description={costs[1].description} amount={costs[1].amount} />
+     <CostItem date={costs[2].date} description={costs[2].description} amount={costs[2].amount} />
     </div>
   );
 }
