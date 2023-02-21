47131307b574c0e28cfcb325da326cc2351aa1fb
Alex <velengura.alex@gmail.com>
2023-02-21 12:12:59 +0200

    simple setState example

--- src/components/Costs/CostItem.js
+++ src/components/Costs/CostItem.js
@@ -1,13 +1,22 @@
+import { useState } from 'react';
 import Card from '../UI/Card';
 import CostDate from './CostDate';
 import './CostItem.css'
 const CostItem = (props) => {
+    
+    const [description, setDescription] = useState(props.description);
+    
+    const ChangeDescriptionHandler = () => {
+        setDescription("Test hook change");
+    }
+
     return (
         <Card className='cost-item'>            
             <CostDate date={props.date} />
             <div className='cost-item__description'>
-                <h2>{props.description}</h2>
+                <h2>{description}</h2>
                 <div className='cost-item__price'>${props.amount}</div>
+                <button onClick={ChangeDescriptionHandler}>change description</button>
             </div>
         </Card>
     )
