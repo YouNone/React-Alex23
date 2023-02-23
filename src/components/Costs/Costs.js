b35ddb8430f6810a7f286bdf796d44d5f132f7ca
Alex <velengura.alex@gmail.com>
2023-02-23 18:21:58 +0200

    created CostList component, refactoring, dividing logic

--- src/components/Costs/Costs.js
+++ src/components/Costs/Costs.js
@@ -1,8 +1,8 @@
-import CostItem from './CostItem';
 import './Costs.css';
 import Card from '../UI/Card';
 import CostsFilter from './CostsFilter';
 import { useState } from 'react';
+import CostList from './CostList';
 
 const Costs = (props) => {
     const [selectedYear, setYear] = useState('2023');
@@ -14,18 +14,7 @@ const Costs = (props) => {
     }
 
     //  forth solution, final
-    let costContent = <p>No purchases this year</p>;
-
-    if (filteredCosts.length > 0) {
-        costContent = filteredCosts.map(cost =>
-            <CostItem 
-                key={cost.id} 
-                date={cost.date} 
-                description={cost.description}
-                amount={cost.amount}
-            />
-        );
-    }
+   
 
     // Part third ternat solution before return
     // let costContent = filteredCosts.length === 0 ? <p>No purchases this year</p> : filteredCosts.map(cost =>
@@ -41,7 +30,6 @@ const Costs = (props) => {
        <div>
          <Card className='costs'>
             <CostsFilter year={selectedYear} onChangeYear={onChangeYearHandler}/>
-
             {/*!! frist hard to read code */}
 
             {/* {filteredCosts.length === 0 && <p>No purchases this year </p>} 
@@ -69,7 +57,8 @@ const Costs = (props) => {
             } */}
 
             {/* third/ forth solution */}
-            {costContent}
+            {/* {costContent} */}
+            <CostList costs={filteredCosts} />
 
         </Card>
        </div>
