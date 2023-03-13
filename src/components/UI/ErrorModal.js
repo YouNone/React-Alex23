08fe9edec5413b6087e4046ec209d3028c429c51
Alex <velengura.alex@gmail.com>
2023-03-13 12:26:02 +0200

    ErrorModal rework, added portals to backdrop and modal dialog

--- src/components/UI/ErrorModal.js
+++ src/components/UI/ErrorModal.js
@@ -1,24 +1,47 @@
 import Card from "./Card";
+import Button from "./Button";
 import styles from "./ErrorModal.module.css";
-import Button from "./Button.js";
+import React from "react";
+import ReactDOM from "react-dom";
 
 const ErrorModal = (props) => {
-  return (
-    <div>
-      <div className={styles.backdrop} onClick={props.onCloseModal}></div>
+  const Backdrop = (props) => {
+    return <div className={styles.backdrop} onClick={props.onCloseModal}></div>;
+  };
 
+  const Modal = (props) => {
+    return (
       <Card className={styles.modal}>
         <header className={styles.header}>
           <h2>{props.title}</h2>
         </header>
         <div className={styles.content}>
-          <h3>{props.message}</h3>
+          <p>{props.message}</p>
         </div>
         <footer className={styles.actions}>
           <Button onClick={props.onCloseModal}>Close</Button>
         </footer>
       </Card>
-    </div>
+    );
+  };
+
+  return (
+    <React.Fragment>
+      {ReactDOM.createPortal(
+        <Backdrop onCloseModal={props.onCloseModal} />,
+        document.getElementById("backdrop")
+      )}
+      {ReactDOM.createPortal(
+        <Modal
+          title={props.title}
+          message={props.message}
+          onCloseModal={props.onCloseModal}
+        />,
+        document.getElementById("modal")
+      )}
+      {/* <Backdrop />
+      <Modal /> */}
+    </React.Fragment>
   );
 };
 
